446406c7bc99c0a14e8a4162d72ed73580818c41
nunziopanzarell <nunziopanzarell@gmail.com>
2025-11-15 17:27:17 +0200

    initial commit

--- command.py
+++ command.py
@@ -18,54 +18,63 @@ async def on_ready():
 
 ####
 
-culture_nugget1=\
-     {"ID": 1,
- "Q": "Who is considered the father of computer science?",
- "A": "Alan Turing"
- }
-
-culture_nugget2=\
-     {"ID": 2,
-  "Q": "What does HTML stand for?",
-  "A": "HyperText Markup Language"}
-
-
-culture_nugget3=\
-   {"ID": 3,
-    "Q": "What does the file extension “.jpg” indicate?",
- "A": "It is a compressed image file format"}
-
-culture_nugget4=\
-    {"ID": 4,
- "Q": "What is the name of the inventor of the World Wide Web?",
- "A": "Tim Berners-Lee"}
-
-culture_nugget5=\
-   {"ID": 5,
- "Q": "What does RAM mean?",
- "A": "Random Access Memory"}
-
-culture_nuggets= [
- culture_nugget1,
- culture_nugget2,
- culture_nugget3,
- culture_nugget4,
- culture_nugget5
- ]
-
-with open("data.json", "w") as file:
-     culture_nuggets=json.dump(culture_nuggets, file)
-
-print(culture_nuggets)
+# culture_nugget1=\
+#      {"ID": 1,
+#  "Q": "Who is considered the father of computer science?",
+#  "A": "Alan Turing"
+#  }
+#
+# culture_nugget2=\
+#      {"ID": 2,
+#   "Q": "What does HTML stand for?",
+#   "A": "HyperText Markup Language"}
+#
+#
+# culture_nugget3=\
+#    {"ID": 3,
+#     "Q": "What does the file extension “.jpg” indicate?",
+#  "A": "It is a compressed image file format"}
+#
+# culture_nugget4=\
+#     {"ID": 4,
+#  "Q": "What is the name of the inventor of the World Wide Web?",
+#  "A": "Tim Berners-Lee"}
+#
+# culture_nugget5=\
+#    {"ID": 5,
+#  "Q": "What does RAM mean?",
+#  "A": "Random Access Memory"}
+#
+# culture_nuggets= [
+#  culture_nugget1,
+#  culture_nugget2,
+#  culture_nugget3,
+#  culture_nugget4,
+#  culture_nugget5
+#  ]
+#
+# with open("data.json", "w") as file:
+#      culture_nuggets=json.dump(culture_nuggets, file)
+#
+# print(culture_nuggets)
 
 ######
 
 with open("data.json", "r") as file:
     culture_nuggets=json.load(file)
 
-pick_random=random.choice(culture_nuggets)
-print(pick_random["Q"])
-print(pick_random["A"])
+# pick_random=random.choice(culture_nuggets)
+# print(pick_random["Q"])
+# print(pick_random["A"])
+
+@bot.command()
+async def nugget(ctx):
+    pick_random = random.choice(culture_nuggets)
+    question = pick_random["Q"]
+    answer = pick_random["A"]
+
+    await ctx.send(f"💡 **Did you know?**\n {question}\n **Answer:** {answer}")
+
 
 bot.run(TOKEN)
 
